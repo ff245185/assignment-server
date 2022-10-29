81b2d4e94ce05c7fbb6f5141cc964ca9917872e0
abdullah <ff245185@gmail.com>
2022-10-30 01:03:49 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.Port || 5000;
+app.use(cors());
+const categories = require("./data/catagroy.json");
+const courses = require("./data/generated.json");
+app.get("/", (req, res) => {
+  res.send("Courses Api Running");
+});
+
+app.get("/courses-categories", (req, res) => {
+  res.send(categories);
+});
+app.get("/courses-categories/:id", (req, res) => {
+  const id = req.params.id;
+  const coursesCategories = courses.find((c) => c.id == id);
+  res.send(coursesCategories);
+});
+
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
+
+app.get("/courses/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedCourses = courses.find((course) => course.id === id);
+  res.send(selectedCourses);
+});
+
+app.listen(port, () => {
+  console.log("learning programming server on port", port);
+});
